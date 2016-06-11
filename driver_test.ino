1e3f6980d10a917bd6ec4f6ab1aef86eda9cbc42
nicola <nicola@HAL.(none)>
2016-06-11 20:08:11 +0200

    first commit

---
+++ driver_test.ino
@@ -0,0 +1,48 @@
+//Info diver
+//http://papcodientu.com/store/motor-driver-tb6612.html
+//Info PWM
+//https://www.arduino.cc/en/Tutorial/PWM
+
+//Set board to: arduino pro or pro mini 5V
+
+int pwm = 9;
+int ain1 = 13;
+int ain2 = 12;
+
+
+
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(pwm, OUTPUT);     
+  pinMode(ain1, OUTPUT);     
+  pinMode(ain2, OUTPUT);     
+}
+
+void loop() {
+  
+  //Stop
+  digitalWrite(pwm, HIGH);   // set the LED on
+  digitalWrite(ain1, HIGH);   // set the LED on
+  digitalWrite(ain2, HIGH);   // set the LED on
+  delay(2000);
+  
+  //Forward
+  digitalWrite(pwm, HIGH);   // set the LED on
+  digitalWrite(ain1, LOW);   // set the LED on
+  digitalWrite(ain2, HIGH);   // set the LED on
+  delay(2000);
+
+  //Back
+  digitalWrite(pwm, HIGH);   // set the LED on
+  digitalWrite(ain1, HIGH);   // set the LED on
+  digitalWrite(ain2, LOW);   // set the LED on
+  delay(2000);
+  
+  //Acceleration
+  for(int i=0;i<256;i++) {
+    analogWrite(pwm,i);
+    delay(30);
+  }
+  
+
+}
